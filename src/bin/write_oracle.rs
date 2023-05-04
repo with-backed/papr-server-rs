0f131a8b9d94d7f0373145871be219ebe7fd214b
Wilson Cusack <cusackw@gmail.com>
2023-05-04 14:45:55 -0400

    allow controller passed to write_oracle

--- src/bin/write_oracle.rs
+++ src/bin/write_oracle.rs
@@ -1,8 +1,17 @@
 use papr_server_rs::oracle;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), eyre::Error> {
-    oracle::cache_oracle_values("0x3b29c19ff2fcea0ff98d0ef5b184354d74ea74b0").await?;
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} <address>", args[0]);
+        std::process::exit(1);
+    }
+
+    let controller = &args[1];
+    oracle::cache_oracle_values(controller).await?;
 
     Ok(())
 }
