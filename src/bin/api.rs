03ef2a4199ef50469582add63059fcbd0f4447e1
Wilson Cusack <cusackw@gmail.com>
2023-05-04 16:43:28 -0400

    allow cors

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -1,15 +1,32 @@
 use papr_server_rs::api::routes::{info, oracle};
+use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
+use std::str::FromStr;
 
 #[macro_use]
 extern crate rocket;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount(
-        "/",
-        routes![
-            info::get,
-            oracle::oracle_info_for_controller_allowed_collateral
-        ],
-    )
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::All)
+        .allowed_methods(
+            vec!["Get"]
+                .into_iter()
+                .map(|s| FromStr::from_str(s).unwrap())
+                .collect(),
+        )
+        .allowed_headers(AllowedHeaders::All)
+        .allow_credentials(true)
+        .to_cors()
+        .expect("Error initializing CORS");
+
+    rocket::build()
+        .mount(
+            "/",
+            routes![
+                info::get,
+                oracle::oracle_info_for_controller_allowed_collateral
+            ],
+        )
+        .attach(cors)
 }
